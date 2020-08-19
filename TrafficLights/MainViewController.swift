cbf3bc4f1a5e2b8ccd496e8c38e198965c4f22bc
Vladislav <sfx.nontage@gmail.com>
2020-08-19 15:30:34 +0300

    Add function

--- TrafficLights/MainViewController.swift
+++ TrafficLights/MainViewController.swift
@@ -13,6 +13,7 @@ class MainViewController: UIViewController {
     var stepClickButton = 0
 
     @IBOutlet var blockIndecatorsView: [UIView]!
+    
     @IBOutlet weak var ChangeColorButton: UIButton!
     
     
@@ -21,39 +22,52 @@ class MainViewController: UIViewController {
         
         ChangeColorButton.layer.cornerRadius = 10
         roundingElements(blockView: blockIndecatorsView)
-
+    }
+    
+    // ослеживаем поворот экрана
+    override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
+        roundingElements(blockView: blockIndecatorsView)
     }
 
+    // отслеживаем нажатие кнопки
     @IBAction func ChangeColorActionButon() {
         stepClickButton += 1
         if stepClickButton == blockIndecatorsView.count + 1 {
             stepClickButton = 1
         }
-        print(stepClickButton)
+        
         changeColor(blockView: blockIndecatorsView, step: stepClickButton)
     }
 
-    
-    override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
-        getScreenSize()
-    }
-    
-    func getScreenSize() {
-        roundingElements(blockView: blockIndecatorsView)
-    }
-    
-    func roundingElements(blockView: [UIView]) {
+    // применяем скругление элементов массива View
+    func roundingElements(blockView: [UIView]?) {
+        guard let blockView = blockView else { return }
         blockView.forEach { elem in
             elem.layer.cornerRadius = elem.bounds.height / 2
         }
     }
     
-    func changeColor(blockView: [UIView], step: Int) {
-        blockView.forEach { (elem) in
+    // меняем прозрачность элементов View после нажатия кнопки
+    func changeColor(blockView: [UIView]?, step: Int) {
+        guard let blockView = blockView else { return }
+        blockView.forEach { elem in
             elem.alpha = 0.3
+            elem.layer.shadowOpacity = 0.0
         }
-        blockView[step - 1].alpha = 1
+        
+        let elemView = blockView[step - 1]
+        elemView.alpha = 1
+        
+        shadowLight(blockView: elemView)
+    }
+    
+    // прорисовка тени
+    func shadowLight(blockView: UIView?) {
+        guard let elem = blockView else { return }
+        elem.layer.shadowColor = blockView?.backgroundColor?.cgColor
+        elem.layer.shadowRadius = 5
+        elem.layer.shadowOpacity = 1
+        elem.layer.shadowOffset = CGSize(width: 0, height: 0)
     }
 }
-
 
